93527333b4a03beb63508dbbb7caf08fb64dfff1
Dharmaraj001 <dharmarajs877@gmail.com>
2025-11-01 13:09:44 +0530

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 class="text-3xl font-bold underline bg-amber-200">Hello</h1>
+    </div>
+  )
+}
+
+export default App
