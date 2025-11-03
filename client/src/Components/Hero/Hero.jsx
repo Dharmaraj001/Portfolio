3b53afd494f67b776de7540031eb235e604f48e7
Dharmaraj001 <dharmarajs877@gmail.com>
2025-11-03 16:02:22 +0530

    Hero page created(Demo)

--- client/src/Components/Hero/Hero.jsx
+++ client/src/Components/Hero/Hero.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import {BrowserRouter, Link} from "react-router-dom"
 
 const Hero = () => {
     return(
-        <h1>Hero</h1>
+        <div className='flex items-center justify-center'>
+            <button className='bg-white/10 text-xl text-red-400'>
+            <Link to="/about">Enter the universe</Link>
+            </button>
+        </div>
     )
 }
 
