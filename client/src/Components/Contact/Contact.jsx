3addb0cbc9e6402925efaaa396ee7c672a78309d
Dharmaraj001 <dharmarajs877@gmail.com>
2025-11-04 23:13:09 +0530

    Contact form updated

--- client/src/Components/Contact/Contact.jsx
+++ client/src/Components/Contact/Contact.jsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 import { FiMail, FiMapPin, FiGithub, FiLinkedin, FiTwitter, FiUser, FiMessageSquare, FiSend  } from "react-icons/fi";
-import { FaDiscord } from "react-icons/fa";
+import { FaDiscord, FaInstagram  } from "react-icons/fa";
+import { motion } from "framer-motion";
 
 
 const Contact = () => {
+
+  const [result, setresult] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setresult("Sending...")
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "812a6d23-dcf3-48f0-ad3d-1f6643466cc8")
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if(data.success){
+      setresult("Message sent successfully ✅")
+      event.target.reset();
+    }else {
+      console.log("Error", data);
+      setresult("Something went wrong ❌");
+    }
+  }
   return (
     <section className="relative z-10 flex flex-col items-center justify-center min-h-screen px-4 py-20 text-white mt-15">
         <h1 className="text-4xl font-bold mb-8">Get in Touch ✨</h1>
@@ -13,29 +39,34 @@ const Contact = () => {
         <div className="bg-white/10 backdrop-blur-md rounded-2xl p-8 border border-white/10 shadow-lg">
           <h2 className="text-md font-semibold text-white-400 mb-4"> <span className="text-sm">🔴🟡🟢 </span>&nbsp; ~/Contact Terminal</h2>
 
-          <form className="space-y-5">
+          <form onSubmit={onSubmit} className="space-y-5">
             <div>
               <label className="text-sm mb-1.5 flex"> &nbsp; <FiUser size={22}/> &nbsp; Name</label>
               <input
-                type="text"
+                type="text" 
+                required={true}
+                name="name"
                 placeholder="Enter your name..."
                 className="w-full p-3 bg-white/10 border border-white/20 rounded-xl focus:outline-none focus:ring-2 focus:ring-gray-400"
               />
             </div>
 
             <div>
-              <label className="text-sm mb-1.5 flex"> &nbsp; <FiMail size={22}/> &nbsp; Email</label>
+              <label className="text-sm mb-1.5 flex"> &nbsp; <FiMail size={22}/> &nbsp; Email / Linkedin</label>
               <input
                 type="email"
+                required={true}
+                name="email"
                 placeholder="your@email.com"
                 className="w-full p-3 bg-white/10 border border-white/20 rounded-xl focus:outline-none focus:ring-2 focus:ring-gray-400"
               />
             </div>
 
             <div>
-              <label className="text-sm mb-1.5 flex"> &nbsp; <FiMessageSquare size={22}/> &nbsp; Mesage</label>
+              <label className="text-sm mb-1.5 flex"> &nbsp; <FiMessageSquare size={22}/> &nbsp; Message</label>
               <textarea
-                placeholder="Transmit your message..."
+              name="message"
+                placeholder="Transmit your thought..."
                 rows="4"
                 className="w-full p-3 bg-white/10 border border-white/20 rounded-xl focus:outline-none focus:ring-2 focus:ring-gray-400"
               ></textarea>
@@ -43,10 +74,11 @@ const Contact = () => {
 
             <button
               type="submit"
-              className=" flex text-center justify-center w-full bg-gradient-to-r from-purple-500 to-blue-400 text-white py-3 rounded-xl font-semibold hover:from-purple-600 hover:to-blue-500 transition-all duration-300" 
+              className=" flex text-center justify-center w-full bg-gradient-to-r from-purple-500 to-blue-400 text-white py-3 rounded-xl font-semibold hover:from-purple-600 hover:to-blue-500 hover:cursor-pointer transition-all duration-300" 
                 > <span className="mt-1"><FiSend/></span> &nbsp; Send Message
             </button>
           </form>
+          {result && <p className="mt-4 text-gray-300">{result}</p>}
         </div>
 
         {/* Right: Info Section */}
@@ -70,13 +102,13 @@ const Contact = () => {
           <div className="bg-white/10 backdrop-blur-md rounded-2xl p-6 border border-white/10 shadow-lg">
             <h3 className="font-semibold text-lg mb-4">Connect Across the Universe</h3>
             <div className="flex gap-4">
-              <a href="#" className="p-2 bg-white/10 rounded-lg hover:bg-white/20 transition">
+              <a href="https://github.com/" target="_blank" className="p-2 bg-white/10 rounded-lg hover:bg-white/20 transition">
                 <FiGithub size={20} />
               </a>
-              <a href="#" className="p-2 bg-white/10 rounded-lg hover:bg-white/20 transition">
-                <FiLinkedin size={20} />
+              <a href="https://www.instagram.com/dh_arm_a_raj/?hl=en" target="_blank" className="p-2 bg-white/10 rounded-lg hover:bg-white/20 transition">
+                <FaInstagram  size={20} />
               </a>
-              <a href="#" className="p-2 bg-white/10 rounded-lg hover:bg-white/20 transition">
+              <a href="https://x.com/Dharmaraj_SS" target="_blank" className="p-2 bg-white/10 rounded-lg hover:bg-white/20 transition">
                 <FiTwitter size={20} />
               </a>
             </div>
@@ -92,6 +124,13 @@ const Contact = () => {
           </div>
         </div>
       </div>
+      <motion.div 
+        initial={{opacity:0, y:20}}
+        animate={{opacity:1, y:0}}
+        transition={{duration:1.2, delay:1}}
+        className="absolute bottom-2 text-xs sm:text-sm text-gray-500 tracking-wide">
+        Built with <span className="text-red-500">❤️</span> by <span className="text-gray-400 font-bold hover:text-gray-300">Dharmaraj</span>
+        </motion.div>
     </section>
   );
 };
