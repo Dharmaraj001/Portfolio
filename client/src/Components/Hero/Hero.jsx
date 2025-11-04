31c9e40db387fc5a1ce9a16c80983b99f9bc29b8
Dharmaraj001 <dharmarajs877@gmail.com>
2025-11-04 21:29:37 +0530

    Hero section created

--- client/src/Components/Hero/Hero.jsx
+++ client/src/Components/Hero/Hero.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import {Link} from "react-router-dom"
 import { TypeAnimation } from 'react-type-animation';
+import { motion } from "framer-motion";
 
 const Hero = () => {
   return (
-    <div className='flex flex-col bg-black items-center justify-center h-screen text-white text-center overflow-hidden'>
+    <div className='flex start flex-col bg-black items-center justify-center h-screen text-white text-center overflow-hidden'>
         {/* Heading */}
-        <h1 className='text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-semibold mb-3 leading-snug'>
-            <span className='text-4xl sm:text-5xl md:text-6xl'>🌐</span>Welcome to <span className='text-indigo-400'>Space</span> <br className='block sm:hidden'/> themed portfolio
-        </h1>
+        <motion.h1 
+                initial={{ opacity: 0, y: -30 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 1 }}
+        className='text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-semibold mb-3 leading-snug'>
+            <span className='text-4xl sm:text-5xl md:text-6xl' style={{display:"inline-block", animation: "spin 6s linear infinite"}}>🌐</span>Welcome to <span className='relative bg-gradient-to-r from-yellow-200 via-orange-400 to-pink-500200%_200%] animate-gradientMove bg-clip-text text-transparent font-extrabold'>Space</span> <br className='block sm:hidden'/> themed portfolio
+        </motion.h1>
 
         {/* Tag lines */}
         <TypeAnimation
@@ -27,16 +32,41 @@ const Hero = () => {
             />
 
         {/* Skills */}
-        <div className='mt-5 bg-white/10 border border-white/40 rounded-2xl p-3 text-sm sm:text-base md:text-lg text-gray-300 mb-5 backdrop-blur-sm shadow-md'>
-                React • HTML • CSS • Tailwind • JavaScript
-        </div>
+
+        <motion.div 
+            initial={{opacity:0, y:-30}}
+            animate={{opacity:1, y:0}}
+            transition={{duration:1}}
+        className='mt-5 bg-white/10 border border-white/40 rounded-2xl p-3 text-sm sm:text-base md:text-lg text-gray-300 mb-5 backdrop-blur-sm shadow-md hover:bg-white/15'>
+            Built with <span className='text-[#75C2D9] font-bold'>React </span> <span className='font-bold'>+</span> <span className='text-yellow-300 font-bold'> Framer Motion </span><span className='font-bold'>+</span> <span className='font-bold text-[#4EB9FA]'>Tailwind</span>
+        </motion.div>
 
         
-        <div className='mt-1'>
-            <Link to="/about" className=' border border-white/30 rounded-md py-2 px-5 bg-white/10 hover:bg-white/30 transition duration-300 text-sm sm:text-base'>
-                Enter
+        <motion.div 
+            initial={{opacity:0, y:30}}
+            animate={{opacity:1}}
+            transition={{duration:1}}
+        className='mt-3'>
+            <Link 
+            to="/about"
+            className="border border-indigo-400/30 text-white rounded-md py-2 px-5 bg-indigo-400/5 hover:bg-indigo-400/20 hover:shadow-[0_0_2px_rgba(99,102,241,0.6)] transition-all duration-300"
+            >
+            Enter
             </Link>
+        </motion.div>
+
+        <div className=''>
+            <h1 className='mt-30'></h1>
         </div>
+
+        <motion.div 
+        initial={{opacity:0, y:20}}
+        animate={{opacity:1, y:0}}
+        transition={{duration:1.2, delay:1}}
+        className="absolute bottom-3 text-xs sm:text-sm text-gray-500 tracking-wide">
+        Built with <span className="text-red-500">❤️</span> by <span className="text-gray-400 font-bold hover:text-gray-300">Dharmaraj</span>
+        </motion.div>
+
     </div>
   )
 }
